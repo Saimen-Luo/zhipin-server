8d2911622f9c455f80a98067e1a25f1d626e1ca6
Saimen-Luo <lswofficial@163.com>
2019-05-03 19:43:57 +0800

    comment test routes

--- routes/index.js
+++ routes/index.js
@@ -13,18 +13,18 @@ router.get('/', function (req, res, next) {
 // d) admin 是已注册用户
 // e) 注册成功返回: {code: 0, data: {_id: 'abc', username: ‘xxx’, password:’123’}
 // f) 注册失败返回: {code: 1, msg: '此用户已存在'}
-router.post('/register', function (req, res) {
-  // 1. 获取请求参数
-  const { username, password } = req.body
-  // 2. 处理
-  if (username === 'admin') { // 注册失败
-    // 返回响应数据(失败)
-    res.send({code: 1, msg: '此用户已存在'})
-  }else {// 注册成功
-    // 返回响应数据(成功)
-    res.send({code: 0, data: {id: 'abc', username, password}})
-  }
-  // 3. 返回响应数据
-})
+// router.post('/register', function (req, res) {
+//   // 1. 获取请求参数
+//   const { username, password } = req.body
+//   // 2. 处理
+//   if (username === 'admin') { // 注册失败
+//     // 返回响应数据(失败)
+//     res.send({code: 1, msg: '此用户已存在'})
+//   }else {// 注册成功
+//     // 返回响应数据(成功)
+//     res.send({code: 0, data: {id: 'abc', username, password}})
+//   }
+//   // 3. 返回响应数据
+// })
 
 module.exports = router;
