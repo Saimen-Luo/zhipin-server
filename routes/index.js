63cfa136ffd2e463170f2d8a720cdd3c32be3195
Saimen-Luo <lswofficial@163.com>
2019-05-03 20:19:34 +0800

    finish register interface

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const { UserModel } = require('../db/models')
+const md5 = require('blueimp-md5')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -26,5 +28,29 @@ router.get('/', function (req, res, next) {
 //   }
 //   // 3. 返回响应数据
 // })
+
+// 用户注册路由
+router.post('/register', function (req, res) {
+  // 从请求体获取请求参数
+  const { username, password, type } = req.body
+  // 读取已有数据库,判断用户是否已注册;如果已经注册,返回错误信息;如果不存在,保存
+  // 根据username查询
+  UserModel.findOne({ username }, function (error, user) {
+    // 如果有user,说明用户已注册
+    if (user) {
+      res.send({ code: 1, msg: '用户已注册,不能重复注册,请直接登录' })
+    } else { // 用户不存在,new构造函数创建实例并保存,返回成功信息
+      new UserModel({ username, type, password: md5(password) }).save(function (error, user) {
+        // 生成一个 cookie(userid: user._id), 并交给浏览器保存
+        res.cookie('userId', user._id, { maxAge: 1000 * 60 * 60 * 24 }) // 持久化 cookie, 浏览器会保存在本地文件
+        // 返回给用户的信息,需要有_id,且为了安全不能用密码,重新封装一个data
+        const data = { username, type, _id: user.id }
+        res.send({ code: 0, data })
+      })
+    }
+  })
+
+
+})
 
 module.exports = router;
