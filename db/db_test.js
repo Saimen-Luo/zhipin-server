5aea37c056e1a8bd97ff5cc944a1a347dafa0cef
Saimen-Luo <lswofficial@163.com>
2019-05-03 15:55:36 +0800

    fix connect Deprecation Warning

--- db/db_test.js
+++ db/db_test.js
@@ -18,7 +18,7 @@
 // 1.1. 引入 mongoose
 const mongoose = require('mongoose')
 // 1.2. 连接指定数据库 (URL 只有数据库是变化的 )
-mongoose.connect('mongodb://localhost:27017/gzhipin_test')
+mongoose.connect('mongodb://localhost:27017/gzhipin_test', { useNewUrlParser: true })
 // 1.3. 获取连接对象
 const conn = mongoose.connection
 // 1.4. 绑定连接完成的监听 ( 用来提示连接成功 )
